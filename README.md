9226752e6d2c16d14053c9401edd8bc06d681c7f
tracefree <riedev@proton.me>
2025-06-28 15:22:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Short Python script to generate figures of a certain quantum circuit with Qiskit for a university assignment. The circuit is a subroutine of Grover's algorithm, the [phase query gate of the OR function](https://learning.quantum.ibm.com/course/fundamentals-of-quantum-algorithms/grovers-algorithm#phase-query-gates).
+
+## Installation
+
+In a Unix terminal, execute:
+
+```
+git clone bla
+cd bla
+python -m venv venv
+chmod +x ./venv/bin/activate
+./venv/bin/activate
+./venv/bin/pip install -r requirements.txt
+```
+
+## Usage
+
+```
+./venv/bin/python main.py [NUMBER_OF_QUBITS]
+```
+
+Replace `[NUMBER_OF_QUBITS]` by an integer number greater than one. Defaults to 4 if omitted. The program then generates two png images in the working directory, one showing the virtual circuit in abstract terms and the other the same circuit in terms of the [native gate set of IBM's Heron architecture](https://docs.quantum.ibm.com/guides/native-gates#heron).
---
+++ main.py
@@ -0,0 +1,34 @@
+from qiskit_aer import AerSimulator
+from qiskit.circuit.library import RealAmplitudes
+from qiskit.circuit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit.circuit.library import OrGate
+from qiskit import transpile
+import sys
+
+DEFAULT_NUMBER_QUBITS = 4
+
+if __name__ == "__main__":
+    number_qubits = DEFAULT_NUMBER_QUBITS
+    if len(sys.argv) == 2:
+        try:
+            number_qubits = int(sys.argv[1])
+            if number_qubits < 2:
+                number_qubits = DEFAULT_NUMBER_QUBITS
+        except:
+            number_qubits = DEFAULT_NUMBER_QUBITS
+
+    qc = QuantumCircuit(number_qubits)
+    or_gate = OrGate(number_qubits - 1)
+
+    qc.append(or_gate, reversed(range(0, number_qubits)))
+    for i in range(1, number_qubits):
+        qc.cz(0,i)
+
+    qc.draw("mpl", filename="ZOr_virtual_{number_qubits:d}.png".format(number_qubits = number_qubits))
+
+    backend = AerSimulator()
+    basis_gates = ["cz", "rz", "sx", "x", "id", "reset"]
+    qc_compiled = transpile(qc, basis_gates=basis_gates)
+    qc_compiled.draw("mpl", filename="ZOr_compiled_{number_qubits:d}.png".format(number_qubits = number_qubits))
